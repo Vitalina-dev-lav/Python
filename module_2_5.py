da7c9a190904e306f5448d0533883c1a7c6f5a94
Vitalina-dev-lav <vitalinabatrakova@gmail.com>
2024-08-08 21:20:21 +0300

    homework

--- module_2_5.py
+++ module_2_5.py
@@ -39,9 +39,7 @@ def get_matrix(n, m, value):
     return matrix
 
 
-result1 = get_matrix(2, 2, 10)
-result2 = get_matrix(3, 5, 42)
-result3 = get_matrix(4, 2, 13)
-print(result1)
-print(result2)
-print(result3)
+get_matrix(2, 2, 10)
+get_matrix(3, 5, 42)
+get_matrix(4, 2, 13)
+
