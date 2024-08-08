1c2130150167626e37719569f8c13620e5aebbbc
Vitalina-dev-lav <vitalinabatrakova@gmail.com>
2024-08-08 21:47:20 +0300

    homework

--- module_3_2.py
+++ module_3_2.py
@@ -25,17 +25,16 @@
 #За один вызов функции выводится только одно и перечисленных уведомлений!
 # Проверки перечислены по мере выполнения.
 
-def send_email(message, recipient, sender="university.help@gmail.com"):
-    a='@'
-    if recipient.endswith(('.com', '.ru', '.net')) and sender.endswith(('.com', '.ru', '.net')):
-        if a not in recipient and sender:
-            print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
-    if sender == recipient:
+def send_email(message, recipient: str, *, sender="university.help@gmail.com"):
+    domens = (".com", ".ru", ".net")
+    if not ("@" in recipient and "@" in sender and recipient.endswith(domens) and sender.endswith(domens)):
+        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+    elif sender == recipient:
         print("Нельзя отправить письмо самому себе!")
-    if sender == "university.help@gmail.com":
-        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
     elif sender != "university.help@gmail.com":
         print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
+    else:
+        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}")
 
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
