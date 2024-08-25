79daaba7657c2c57d36525fdbf4f19020b97917d
Vitalina-dev-lav <vitalinabatrakova@gmail.com>
2024-08-25 17:29:55 +0300

    homework

--- module_3_hard.py
+++ module_3_hard.py
@@ -28,28 +28,23 @@
 # 1 + 2 + 3 + len('a') + 4 + len('b') + 5 + 6 + len('cube') + 7 + .... + 35 = 99
 
 
-# Создаём функцию подсчёта данных строк и чисел
+
 def calculate_structure_sum(data_structure):
     summa = 0
-    # Прописываем условие при помощи цикла 1 summa для key и value
     if isinstance(data_structure, dict):
         for key, value in data_structure.items():
             summa += calculate_structure_sum(key)
             summa += calculate_structure_sum(value)
-            # иначе выполняеться цикл 2 add к summa - список,кортэж,множества
     elif isinstance(data_structure, (list, tuple, set)):
         for item in data_structure:
             summa += calculate_structure_sum(item)
-            # иначе выполняеться действие 3 add к summa - целое число и число с плавающей запятой
     elif isinstance(data_structure, (int, float)):
         summa += data_structure
-        # иначе выполняеться действие 4 add k summa - фу-цию len
     elif isinstance(data_structure, str):
         summa += len(data_structure)
-        # возврат из функции значения summa
     return summa
 
-# данные взятые из условия задачи
+
 data_structure = [
   [1, 2, 3],
   {'a': 4, 'b': 5},
@@ -57,6 +52,6 @@ data_structure = [
   "Hello",
   ((), [{(2, 'Urban', ('Urban2', 35))}])
 ]
-# вывод результата программы
+
 result = calculate_structure_sum(data_structure)
 print(result)
