111733f7f861f65a53be06bdd14843876850dda3
Vitalina-dev-lav <vitalinabatrakova@gmail.com>
2024-10-13 17:43:12 +0300

    homework

---
+++ Module7/module_7_1.py
@@ -0,0 +1,56 @@
+# Задача "Учёт товаров":
+# Необходимо реализовать 2 класса Product и Shop,
+# с помощью которых будет производиться запись в файл с продуктами.
+# Объекты класса Product будут создаваться следующим образом - Product('Potato', 50.0, 'Vagetables')
+# и обладать следующими свойствами:
+# Атрибут name - название продукта (строка).
+# Атрибут weight - общий вес товара (дробное число) (5.4, 52.8 и т.п.).
+# Атрибут category - категория товара (строка).
+# Метод __str__, который возвращает строку в формате '<название>, <вес>, <категория>'.
+# Все данные в строке разделены запятой с пробелами.
+
+from pprint import pprint
+
+
+class Product:
+    def __init__(self, name: str, weight: float, category: str):
+        self.name = name
+        self.weight = weight
+        self.category = category
+
+    def __str__(self):
+        return (f'{self.name}; {self.weight}; {self.category}')
+
+
+class Shop:
+    def __init__(self):
+        self.__file_name = 'products.txt'
+
+    def get_products(self):
+        self.__file_name = 'products.txt'
+        file = open(self.__file_name, 'r')
+        products = file.read()
+        file.close()
+
+        return products
+
+    def add(self, *products):
+        for product in products:
+            if product.name not in self.get_products():
+                file = open(self.__file_name, 'a')
+                file.write(product.__str__() + '\n')
+
+            else:
+                print(f'Продукт {product.name} уже есть в магазине')
+
+
+s1 = Shop()
+p1 = Product('Potato',  50.5, 'Vegitables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
+s1.add(p1, p2, p3)
+s1.get_products()
+print(p1)
+print(p2)
+print(p3)
+
