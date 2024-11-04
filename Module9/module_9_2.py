674098726b1532161b86ce253d71d53e1e1517fa
Vitalina-dev-lav <vitalinabatrakova@gmail.com>
2024-11-04 19:02:03 +0300

    homework

--- Module9/module_9_2.py
+++ Module9/module_9_2.py
@@ -23,3 +23,8 @@ third_result = {x: len(x) for x in (first_strings + second_strings) if not len(x
 print(first_result)
 print(second_result)
 print(third_result)
+
+# Вывод на консоль:
+# [10, 8, 8]
+# [('Elon', 'Task'), ('Elon', 'Java'), ('Musk', 'Task'), ('Musk', 'Java'), ('Monitors', 'Computer'), ('Variable', 'Computer')]
+# {'Elon': 4, 'Musk': 4, 'Programmer': 10, 'Monitors': 8, 'Variable': 8, 'Task': 4, 'Java': 4, 'Computer': 8}
